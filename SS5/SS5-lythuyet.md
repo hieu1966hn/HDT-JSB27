7a992fcdeefecf7707deabe0653f2677c8ef8e87
hieu1966hn <hieu1966hn@gmail.com>
2022-01-09 20:44:29 +0700

    Done JS ly thuyet

---
+++ SS5/SS5-lythuyet.md
@@ -0,0 +1,14 @@
+Javascript là gì? 
+
+Lập trình web chia làm 2 nhánh lớn: 
+
+Front-end (UI: Giao diện, client)         Back-end (Server)               APP
+ReactJS(Thư viện front-end)               NodeJS (trình biên dịch JS)     Electron, RN
+Facebook, instagram,                                                      Messenger,
+Shopee,...                                                                VScode
+
+Javascript (JS)                           JS                              JS
+
+
+
+
---
+++ SS5/app.js
@@ -0,0 +1,25 @@
+// Kiểu dữ liệu cơ bản trong JS
+
+// Number: Kiểu số. VD: 1,2,3,4,5
+// Boolean: true/false
+// String: 'Hello World', "Hello", `Hi`
+// Null: null: Nó là kiểu dữ liệu rỗng: VD
+// Undefined: Kiểu không xác định: VD
+// Object: Đối tượng: {name: "Hieu", age: "24"}
+
+
+////// KHái niệm biến trong JS: Biến dùng để lưu trữ dữ liệu.
+//// Khai báo biến: let/var/const
+
+// let: khai báo biến bình thường
+let a = 123;
+a = 321
+
+// var: khai báo biến bình thường
+var b = "Đây là biến b";
+
+// const: KHai báo hằng số.
+const Pi = 3.14;
+// Pi = 4.14; // Sai vì hằng số là số không đổi.
+
+// NX: Let/var thay đổi được. Hằng số thì không.
