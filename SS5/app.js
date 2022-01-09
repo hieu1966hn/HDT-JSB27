e52f3e47540e7e1a5a48645c307cf67f452e5e2c
hieu1966hn <hieu1966hn@gmail.com>
2022-01-09 21:04:11 +0700

    done toán tử số học

--- SS5/app.js
+++ SS5/app.js
@@ -7,19 +7,64 @@
 // Undefined: Kiểu không xác định: VD
 // Object: Đối tượng: {name: "Hieu", age: "24"}
 
-
 ////// KHái niệm biến trong JS: Biến dùng để lưu trữ dữ liệu.
 //// Khai báo biến: let/var/const
 
 // let: khai báo biến bình thường
-let a = 123;
-a = 321
+// let a = 123;
+// a = 321
 
 // var: khai báo biến bình thường
-var b = "Đây là biến b";
+// var b = "Đây là biến b";
 
 // const: KHai báo hằng số.
-const Pi = 3.14;
+// const Pi = 3.14;
 // Pi = 4.14; // Sai vì hằng số là số không đổi.
 
 // NX: Let/var thay đổi được. Hằng số thì không.
+
+// Lưu ý: câu lệnh: "console.log()": là câu lệnh in giá trị ra màn hình console trên trình duyệt
+
+////// Luyện tập với toán tử trong JS:
+// console.log("Đây là câu lệnh Console.log dùng để in ra cái gì mình thích trên cửa sổ console tại trình duyệt");;
+
+///// Toán tử số học: +, -, *, /
+// phép +
+// console.log(1+2);
+// console.log(1000+1);
+
+// phép -
+// console.log(1-2);
+// console.log(101-1);
+
+// phép *
+// console.log(9 * 8); // 72
+// console.log(3 * -1); // -3
+
+// phép /
+// console.log(3/2); // 1.5
+// console.log(1/0); // Infinity
+
+// phép %
+// console.log(56%3);
+// let chiaLayDu = 5%4; //1
+// console.log("chiaLayDu của 5%4 là: ", chiaLayDu);
+
+// phép ++
+// let i = 1;
+// i++; // tăng 1 đơn vị
+// console.log(i);
+
+// let phepToanCongCong = 3 + i++ + ++i + 1; // 8,
+//    3 +   2   +   2   + 1 // sai
+//    3 +   1   +   3   + 1 // Đúng
+// console.log("phepToanCongCong: ", phepToanCongCong);
+
+// phép --: giảm 1 đơn vị
+// let j = 5;
+// j--;
+// console.log(j); //4
+// let k = 5;
+// --k;
+// console.log(k);
+
