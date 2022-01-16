1beee0edee02e9d53cdda0ccd3f0c7579c33fada
hieu1966hn <hieu1966hn@gmail.com>
2022-01-16 20:23:26 +0700

    done do - while

--- SS6/index.js
+++ SS6/index.js
@@ -1,5 +1,5 @@
 // Bắt đầu code JS tại đây:
-/////// Vòng lặp cơ bản
+/////// Vòng lặp cơ bản: while, (do -  while), for
 
 //// While
 // cú pháp:
@@ -16,12 +16,37 @@ B3: Điều kiện sai -> thoát vòng lặp
 
 */
 
-// VD: In ra các số từ 1 -> 10;
-let i = 1; // i = 1
-while(i < 11){ // true
-    console.log(i); // 1,1,1
+// // VD: In ra các số từ 1 -> 10;
+// let i = 1; // i = 1
+// while(i < 11){ // true
+//     console.log(i); // 1,1,1
 
-    // Sau mỗi lần lặp. Biến i sẽ tăng 1 đơn vị 
-    i = i+1; // i++
+//     // Sau mỗi lần lặp. Biến i sẽ tăng 1 đơn vị 
+//     i = i+1; // i++
+// }
 
+
+/////// do - while: 
+//// Cú pháp:
+/*
+do{
+    // code javascript
 }
+while(điều kiện);
+
+
+Mô tả cách hoạt động:
+B1: Cho phép chạy code trong "do" lần đầu tiên
+B2: Kiểm tra điều kiện
+B3: Điều kiện đúng => chạy code trong "do"  => Quay lại bước 2
+B4: Điều kiện sai => Thoát vòng lặp
+*/
+
+// VD: in ra các số từ 1 -> 10 với vòng lặp do - while
+let i =1; // khai báo biến i
+do{
+// in ra số 1 luôn đi
+console.log(i); // 1,2 ,.., 10
+i = i + 1;
+}
+while(i < 11);
