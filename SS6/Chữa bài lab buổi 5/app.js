8447d2814e2474e1e4014e077e96a4ba5f2a0a52
hieu1966hn <hieu1966hn@gmail.com>
2022-01-16 19:51:19 +0700

    Chữa bài 01 Done

---
+++ SS6/Chữa bài lab buổi 5/app.js
@@ -0,0 +1,14 @@
+/*
+Bài 1:
+*/
+
+let firstName = "Hiếu";
+let surName = "Trung";
+let lastName = "Nguyễn";
+
+// C1:
+console.log(lastName + " " + surName + " " + firstName); // Nguyễn Trung Hiếu
+//          Nguyễn      Trung   Hiếu
+
+//C2:
+alert(lastName + " " + surName + " " + firstName); // Nguyễn Trung Hiếu
