188da6bf5c385c4ba8d65d8c9b806013471042bb
hieu1966hn <hieu1966hn@gmail.com>
2022-02-13 20:05:33 +0700

    done lý thuyết SS*

--- SS8/app.js
+++ SS8/app.js
@@ -1,11 +1,39 @@
 
 /////// Khai báo hàm
-//C1:
-function inRaHelloWorld() {
-  alert("Hello World!!!");
-}
+// //C1:
+// function inRaHelloWorld() {
+//   alert("Hello World!!!");
+// }
 
-// C2:
-let inRaHelloWorld2 = function () {
-  alert("Hello World!!!");
-}
+// // C2:
+// let inRaHelloWorld2 = function () {
+//   alert("Hello World!!!");
+// }
+
+
+// ///// Khai báo Hàm không tham số
+// function demo() {
+//   console.log("Đây là hàm không tham số");
+// }
+
+// ///// Khai báo Hàm có 1 tham số (dưới dạng biến hoặc các kiểu dữ liệu khác)
+// function demo2(x) {
+//   console.log("Đây là hàm 1 tham số là:", x);
+// }
+// ///// Khai báo Hàm có nhiều tham số (dưới dạng biến hoặc các kiểu dữ liệu khác)
+// /*
+// VD:
+// x1 = 1;
+// x2 = "hello";
+// x3 = ["hi"];
+// x4 = {name: "Hiếu"}
+// */
+// function demo2(x1, x2, x3, x4) {
+//   console.log("Đây là hàm nhiều tham số");
+// }
+
+
+// //// Khai báo hàm có Giá trị Trả về
+// function sum() {
+//   return "Đây là hàm có giá trị trả về"
+// }
