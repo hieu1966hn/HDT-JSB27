97f29987e68b8a8363ba3507cb411c544f170729
hieu1966hn <hieu1966hn@gmail.com>
2022-02-13 19:57:33 +0700

    2 cách khai báo hàm

--- SS8/app.js
+++ SS8/app.js
@@ -1,1 +1,11 @@
-alert("Hello World!!!")
+
+/////// Khai báo hàm
+//C1:
+function inRaHelloWorld() {
+  alert("Hello World!!!");
+}
+
+// C2:
+let inRaHelloWorld2 = function () {
+  alert("Hello World!!!");
+}
